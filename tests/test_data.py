006db7bba2d6d4f5999ababbbb463481dadd377a
cmdoret <cyril.mattheydoret@gmail.com>
2023-02-26 01:51:10 +0100

    feat: add collection for abilities and improve card collection

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,17 +6,38 @@ import unittest
 from pathlib import Path
 
 from linkml_runtime.loaders import rdf_loader, yaml_loader
-from mtg_ontology.datamodel import Card, Creature
+from mtg_ontology.datamodel import (
+    AbilityCollection,
+    CardCollection,
+)
 
-ROOT = Path(os.path.join(os.path.dirname(__file__), '..'))
+ROOT = Path(os.path.join(os.path.dirname(__file__), ".."))
 
-DATA_DIR =  Path(ROOT) / 'src' / 'data' / 'examples'
+DATA_DIR = Path(ROOT) / "src" / "data" / "examples"
+
 
 class TestData(unittest.TestCase):
     """Test data and datamodel."""
 
     def test_yaml_cards(self):
         """Data test."""
-        for path in DATA_DIR.glob('Card*.yaml'):
-            obj = yaml_loader.load(str(path), target_class=Card)
+        for path in DATA_DIR.glob("Card*.yaml"):
+            obj = yaml_loader.load(str(path), target_class=CardCollection)
             assert obj
+
+    def test_yaml_abilities(self):
+        """Data test."""
+        for path in DATA_DIR.glob("Ability*.yaml"):
+            obj = yaml_loader.load(str(path), target_class=AbilityCollection)
+            assert obj
+
+    def test_land_cards(self):
+        assert yaml_loader.load(
+            str(DATA_DIR / "lands" / "cards.yaml"), target_class=CardCollection
+        )
+
+    def test_land_abilities(self):
+        assert yaml_loader.load(
+            str(DATA_DIR / "lands" / "abilities.yaml"),
+            target_class=AbilityCollection,
+        )
